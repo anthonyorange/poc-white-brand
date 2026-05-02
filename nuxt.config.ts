62219bf5eec058c9cfd7c84f7d24a30cbbe00797
anthony wispelaere <awispelaere@partner.auchan.fr>
2026-05-02 12:11:31 +0200

    fix: add compatibilityDate and test scripts to scaffold

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
   runtimeConfig: {
